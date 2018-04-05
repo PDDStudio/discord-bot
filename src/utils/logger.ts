45335121e874de95d5996243ec082f312ae2716b
Patrick Jung <patrick.pddstudio@googlemail.com>
2018-04-05 17:14:25 +0200

    Added ChatBot requirements to be injected via DI

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -5,24 +5,31 @@ export class Logger implements LoggerFacade {
 
     constructor(private readonly identifier: string) {}
 
+    private stringify(...params: any[]): string {
+        if (Array.isArray(params)) {
+            return params.join(' ');
+        }
+        return params;
+    }
+
     debug(message?: any, ...params: any[]) {
-        console.log(chalk.blue('[DEBUG]'), chalk.italic.grey(this.identifier), message, params);
+        console.log(chalk.blue('[DEBUG]'), chalk.italic.grey(this.identifier), message, this.stringify(params));
     }
 
     error(message?: any, ...params: any[]) {
-        console.log(chalk.red('[ERROR]'), chalk.italic.grey(this.identifier), message, params);
+        console.log(chalk.red('[ERROR]'), chalk.italic.grey(this.identifier), message, this.stringify(params));
     }
 
     info(message?: any, ...params: any[]) {
-        console.log(chalk.blue('[INFO] '), chalk.italic.grey(this.identifier), message, params);
+        console.log(chalk.blue('[INFO] '), chalk.italic.grey(this.identifier), message, this.stringify(params));
     }
 
     log(message?: any, ...params: any[]) {
-        console.log(chalk.blue('[INFO] '), chalk.italic.grey(this.identifier), message, params);
+        console.log(chalk.blue('[INFO] '), chalk.italic.grey(this.identifier), message, this.stringify(params));
     }
 
     warn(message?: any, ...params: any[]) {
-        console.log(chalk.yellow('[WARN] '), chalk.italic.grey(this.identifier), message, params);
+        console.log(chalk.yellow('[WARN] '), chalk.italic.grey(this.identifier), message, this.stringify(params));
     }
 
 }
